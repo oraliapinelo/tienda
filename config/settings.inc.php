89ae4a1a1c187809751a96052d2808c06b3801cc
alumno <alumno>
2016-08-12 00:14:13 -0500

    Actualizacion de base de datos

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,8 +1,8 @@
 <?php
-define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'shopnew');
-define('_DB_USER_', 'root');
-define('_DB_PASSWD_', '');
+define('_DB_SERVER_', 'mysql.hostinger.mx');
+define('_DB_NAME_', 'u827169757_shop');
+define('_DB_USER_', 'u827169757_root');
+define('_DB_PASSWD_', 'oralia2016');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
